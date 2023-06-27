1877f924f463d641bfd6414ca112912e2bd1a305
syndicate <pranitreddyfte@gmail.com>
2023-06-26 19:08:09 -0700

    Segment Class

--- script.js
+++ script.js
@@ -32,4 +32,45 @@ window.onload = function () {
     function dist(p1x, p1y, p2x, p2y) {
       return Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
 }
+class segment {
+  constructor(parent, l, a, first) {
+    this.first = first;
+    if (first) {
+      this.pos = {
+        x: parent.x,
+        y: parent.y
+      };
+    } else {
+      this.pos = {
+        x: parent.nextPos.x,
+        y: parent.nextPos.y
+      };
+    }
+    this.l = l;
+    this.ang = a;
+    this.nextPos = {
+      x: this.pos.x + this.l * Math.cos(this.ang),
+      y: this.pos.y + this.l * Math.sin(this.ang)
+    };
+  }
+
+  update(t) {
+    this.ang = Math.atan2(t.y - this.pos.y, t.x - this.pos.x);
+    this.pos.x = t.x + this.l * Math.cos(this.ang - Math.PI);
+    this.pos.y = t.y + this.l * Math.sin(this.ang - Math.PI);
+    this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+    this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+  }
+
+  fallback(t) {
+    this.pos.x = t.x;
+    this.pos.y = t.y;
+    this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+    this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+  }
+
+  show() {
+    c.lineTo(this.nextPos.x, this.nextPos.y);
+  }
+}
 }
